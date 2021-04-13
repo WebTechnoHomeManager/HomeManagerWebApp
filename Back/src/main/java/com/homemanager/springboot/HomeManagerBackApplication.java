79fdca059a2cc0c43e21e8854a7e137dcd8c2018
Elia <elia.tso@isep.fr>
2021-04-13 09:55:33 +0200

    Springboot starter project

---
+++ Back/src/main/java/com/homemanager/springboot/HomeManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.homemanager.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeManagerBackApplication.class, args);
+	}
+
+}
