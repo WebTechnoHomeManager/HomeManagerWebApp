51f843b4c3f8abbc82029b5663acdcda258bbdd1
mylinhlethien <56452300+mylinhlethien@users.noreply.github.com>
2021-04-13 18:32:02 +0200

    Footer component

--- Front/src/components/Footer.js
+++ Front/src/components/Footer.js
@@ -4,7 +4,9 @@ class Footer extends Component {
     render() {
         return (
             <div class="container-fluid py-2" id="footer">
-                <p>Footer</p>
+                <p>Nous contacter</p>
+                <p>Mentions légales</p>
+                <p>© 2021 Home Manager</p>
             </div>
         )
     }
