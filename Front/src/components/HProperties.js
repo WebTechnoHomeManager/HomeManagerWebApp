9b696773e6e4548228dd8f012624cd2a08996d47
SarahHeo <sarah.heouaine@gmail.com>
2021-04-24 18:19:45 +0200

    Suite accueil

---
+++ Front/src/components/HProperties.js
@@ -0,0 +1,34 @@
+import React, { Component, useState } from 'react';
+import { Form, Button, Row, Col, Accordion, Card } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
+
+import house1 from '../images/houses/house1.jpg';
+import house2 from '../images/houses/house2.jpg';
+import house3 from '../images/houses/house3.jpg';
+import house4 from '../images/houses/house4.jpg';
+
+class HProperties extends Component {
+
+    render() {
+        return (
+            <Row>
+                <Col>
+                    <h4>Paris</h4>
+                    <Image src={house1} className="img-fluid mx-auto d-block" />
+                </Col>
+                <Col>
+                    <h4>Lille</h4>
+                    <Image src={house2} className="img-fluid mx-auto d-block" />
+                </Col>
+                <Col>
+                    <h4>Marseille</h4>
+                    <Image src={house3} className="img-fluid mx-auto d-block" />
+                </Col>
+                <Col>
+                    <h4>Nice</h4>
+                    <Image src={house4} className="img-fluid mx-auto d-block" />
+                </Col>
+            </Row>
+        )
+    }
+} export default HProperties;
