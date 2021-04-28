d4df248710fe562842e9b1656602e351a64033ac
SarahHeo <sarah.heouaine@gmail.com>
2021-04-28 19:46:26 +0200

    header et footer en react-bootstrap, bouton orange

--- Front/src/components/Navbar.jsx
+++ Front/src/components/Navbar.jsx
@@ -1,24 +1,24 @@
 import logo from '../images/logo.png';
 import React, { Component } from 'react';
-import { DropdownButton, Dropdown } from 'react-bootstrap';
+import { Container, Row, Col, Image, DropdownButton, Dropdown, } from 'react-bootstrap';
 
 class Navbar extends Component {
     render() {
         return (
 
-            <div className="container-fluid py-2" id="header">
-                <div className="row">
-                    <img className="col-auto" src={logo} alt="Logo" />
-                    <div className="col" />
-                    <div className="col-auto">FAQ</div>
+            <Container fluid className="py-2" id="header">
+                <Row>
+                    <Image className="col-auto" src={logo} alt="logo" />
+                    <Col></Col>
+                    <Col sm="auto">FAQ</Col>
                     <DropdownButton id="dropdown-basic-button" className="col-auto" title="Connexion" >
                         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
                     </DropdownButton>
-                </div>
+                </Row>
 
-            </div>
+            </Container>
         )
     }
 } export default Navbar;
