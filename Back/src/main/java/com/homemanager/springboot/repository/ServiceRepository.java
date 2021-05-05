009cf8a7f183248acd3abd337cb2e92a7dcf3a73
mylinhlethien <56452300+mylinhlethien@users.noreply.github.com>
2021-05-05 14:38:41 +0200

    remove useless query

--- Back/src/main/java/com/homemanager/springboot/repository/ServiceRepository.java
+++ Back/src/main/java/com/homemanager/springboot/repository/ServiceRepository.java
@@ -8,6 +8,4 @@ import com.homemanager.springboot.model.Service;
 
 public interface ServiceRepository extends CrudRepository<Service, Integer> {
 
-	/*@Query("SELECT service.id, service.name, service.description FROM service JOIN property_services ON service.id = property_services.service_id JOIN property ON property.id = property_services.property_id WHERE property.id = 1")
-	public List<Service> getListServices();*/
 }
