ecf6c2b6aecbbd4550b7171cf2927c5a0cf3a780
Elia <elia.tso@isep.fr>
2021-05-09 20:38:59 +0200

    Add reservations page

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -5,6 +5,7 @@ import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import MyProperties from './pages/MyProperties';
+import MyReservations from './pages/MyReservations';
 import Profile from './pages/Profile';
 import Messaging from './pages/Messaging';
 
@@ -21,6 +22,7 @@ function App() {
                     <Route exact path='/myproperties' component={MyProperties}></Route>
                     <Route exact path='/profile' component={Profile}></Route>
                     <Route exact path='/messaging' component={Messaging}></Route>
+                    <Route exact path='/myreservations' component={MyReservations}></Route>
                 </Switch>
             </div>
             <Footer></Footer>
