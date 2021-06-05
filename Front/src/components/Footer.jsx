b2a27975cb354c6912e36731af14e193d43d28ca
My-Linh Le Thien <mylinh.lethien@gmail.com>
2021-06-05 18:03:11 +0200

    Names in footer

--- Front/src/components/Footer.jsx
+++ Front/src/components/Footer.jsx
@@ -6,7 +6,7 @@ class Footer extends Component {
         return (
 
             <Container fluid className="py-2" id="footer">
-                <p>Contact us</p>
+                <p>Contact us - Sarah HEOUAINE, My-Linh LE THIEN, Elia TSO, Caroline YAN</p>
                 <p>General Conditions of Use</p>
                 <p>© 2021 Home Manager</p>
             </Container>
